f7ce46dd2a16c197b705db04d2fba2ebb5276336
Maheesha <maheeshasithum@gmail.com>
2021-12-26 21:34:27 +0530

    Hours of pure pain and suffering

--- src/main/kotlin/io/github/maheevil/modbot/extensions/moderation/logging/LogCreator.kt
+++ src/main/kotlin/io/github/maheevil/modbot/extensions/moderation/logging/LogCreator.kt
@@ -1,4 +1,31 @@
 package io.github.maheevil.modbot.extensions.moderation.logging
 
-class LogCreator {
+import dev.kord.common.Color
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.behavior.channel.createEmbed
+import dev.kord.core.entity.channel.GuildMessageChannel
+
+/** Reference for future me (color codes)
+ * kicked - 0xff5e00
+ * muted - 0xd9d904
+ * unbanned - 0x09850b
+ * unmuted - 0x55ff00
+ */
+
+suspend fun createModLog(channel: GuildMessageChannel, modAction: String, moderator: Snowflake, target: Snowflake, reason: String?, colour: Color){
+    channel.createEmbed {
+        title = "Member $modAction!"
+        color = colour
+        field("User",false){"<@${target.value}>"}
+        field("Reason",false) {"`${reason ?: "No reason given"}`"}
+        field("Moderator",false){"${channel.getGuild().getMemberOrNull(moderator)?.mention ?: moderator}"}
+    }
+}
+
+suspend fun createAlertLog(){
+
+}
+
+suspend fun createJoinLeaveLog(){
+
 }
--- src/main/kotlin/io/github/maheevil/modbot/extensions/moderation/logging/LogEventListener.kt
+++ src/main/kotlin/io/github/maheevil/modbot/extensions/moderation/logging/LogEventListener.kt
@@ -1,4 +1,41 @@
 package io.github.maheevil.modbot.extensions.moderation.logging
 
-class LogEventListener {
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.event
+import dev.kord.common.Color
+import dev.kord.common.entity.AuditLogEvent
+import dev.kord.core.behavior.getAuditLogEntries
+import dev.kord.core.entity.channel.GuildMessageChannel
+import dev.kord.core.event.guild.BanAddEvent
+import dev.kord.core.event.guild.BanRemoveEvent
+import io.github.maheevil.modbot.modLogsChannelID
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
+
+class LogEventListener : Extension() {
+    override val name = "log"
+
+    override suspend fun setup() {
+        event<BanAddEvent> {
+            action{
+                val ban = event.guild.getAuditLogEntries { AuditLogEvent.MemberBanAdd }
+                        .filter {it.targetId == event.getBan().userId && it.reason == event.getBan().reason }.first()
+
+                if(ban.targetId == null || event.getGuild().getMemberOrNull(ban.userId)?.isBot == true) return@action
+
+                createModLog(event.guild.getChannel(modLogsChannelID) as GuildMessageChannel,"banned", ban.userId, requireNotNull(ban.targetId), ban.reason, Color(0xff0000))
+            }
+        }
+
+        event<BanRemoveEvent> {
+            action {
+                val unban = event.guild.getAuditLogEntries { AuditLogEvent.MemberBanRemove }
+                        .filter {it.targetId == event.user.id}.first()
+                if(unban.targetId == null || event.getGuild().getMemberOrNull(unban.userId)?.isBot == true) return@action
+
+                createModLog(event.guild.getChannel(modLogsChannelID) as GuildMessageChannel,"unbanned", unban.userId, requireNotNull(unban.targetId), unban.reason, Color(0x09850b))
+            }
+        }
+    }
 }
+
