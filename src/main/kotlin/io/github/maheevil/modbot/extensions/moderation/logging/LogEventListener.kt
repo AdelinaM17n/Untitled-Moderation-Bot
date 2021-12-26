08c8456dbc9910c5ce9563a276564ebf607bb57f
Maheesha <maheeshasithum@gmail.com>
2021-12-26 23:50:32 +0530

    changed flow.first() to .firstOrNull()

--- src/main/kotlin/io/github/maheevil/modbot/extensions/moderation/logging/LogEventListener.kt
+++ src/main/kotlin/io/github/maheevil/modbot/extensions/moderation/logging/LogEventListener.kt
@@ -5,13 +5,19 @@ import com.kotlindiscord.kord.extensions.extensions.event
 import dev.kord.common.Color
 import dev.kord.common.entity.AuditLogEvent
 import dev.kord.core.behavior.getAuditLogEntries
+import dev.kord.core.entity.AuditLogEntry
 import dev.kord.core.entity.channel.GuildMessageChannel
 import dev.kord.core.event.guild.BanAddEvent
 import dev.kord.core.event.guild.BanRemoveEvent
+import dev.kord.core.event.guild.MemberLeaveEvent
 import io.github.maheevil.modbot.modLogsChannelID
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.datetime.Clock
 
+@OptIn(kotlin.time.ExperimentalTime::class)
 class LogEventListener : Extension() {
     override val name = "log"
 
@@ -19,9 +25,10 @@ class LogEventListener : Extension() {
         event<BanAddEvent> {
             action{
                 val ban = event.guild.getAuditLogEntries { AuditLogEvent.MemberBanAdd }
-                        .filter {it.targetId == event.getBan().userId && it.reason == event.getBan().reason }.first()
+                        .filter {it.targetId == event.getBan().userId && it.reason == event.getBan().reason
+                                && Clock.System.now().minus(it.id.timestamp).inWholeSeconds < 60}.firstOrNull()
 
-                if(ban.targetId == null || event.getGuild().getMemberOrNull(ban.userId)?.isBot == true) return@action
+                if(ban?.targetId == null || event.getGuild().getMemberOrNull(ban.userId)?.isBot == true) return@action
 
                 createModLog(event.guild.getChannel(modLogsChannelID) as GuildMessageChannel,"banned", ban.userId, requireNotNull(ban.targetId), ban.reason, Color(0xff0000))
             }
@@ -30,12 +37,20 @@ class LogEventListener : Extension() {
         event<BanRemoveEvent> {
             action {
                 val unban = event.guild.getAuditLogEntries { AuditLogEvent.MemberBanRemove }
-                        .filter {it.targetId == event.user.id}.first()
-                if(unban.targetId == null || event.getGuild().getMemberOrNull(unban.userId)?.isBot == true) return@action
+                        .filter {it.targetId == event.user.id && Clock.System.now().minus(it.id.timestamp).inWholeSeconds < 60 }.firstOrNull()
+
+                if(unban?.targetId == null || event.getGuild().getMemberOrNull(unban.userId)?.isBot == true) return@action
+
 
                 createModLog(event.guild.getChannel(modLogsChannelID) as GuildMessageChannel,"unbanned", unban.userId, requireNotNull(unban.targetId), unban.reason, Color(0x09850b))
             }
         }
+
+        /*event<MemberLeaveEvent> {
+            action{
+                val kick = event.guild.getAuditLogEntries { AuditLogEvent.MemberKick }
+            }
+        }*/
     }
 }
 
