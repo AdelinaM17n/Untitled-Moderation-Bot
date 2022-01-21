87b49b3411e9b21bd07ebc648c2bd640cf5397be
Maheesha <maheeshasithum@gmail.com>
2022-01-21 18:55:31 +0530

    Buttons are hard

--- src/main/kotlin/io/github/maheevil/modbot/extensions/util/suggestions/SuggestionCommand.kt
+++ src/main/kotlin/io/github/maheevil/modbot/extensions/util/suggestions/SuggestionCommand.kt
@@ -1,22 +1,20 @@
 package io.github.maheevil.modbot.extensions.util.suggestions
 
 import com.kotlindiscord.kord.extensions.commands.Arguments
-import com.kotlindiscord.kord.extensions.commands.application.slash.PublicSlashCommand
 import com.kotlindiscord.kord.extensions.commands.application.slash.publicSubCommand
 import com.kotlindiscord.kord.extensions.commands.converters.impl.coalescedString
 import com.kotlindiscord.kord.extensions.commands.converters.impl.defaultingCoalescingString
+import com.kotlindiscord.kord.extensions.components.components
+import com.kotlindiscord.kord.extensions.components.publicButton
 import com.kotlindiscord.kord.extensions.extensions.Extension
-import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
-import com.kotlindiscord.kord.extensions.types.respondEphemeral
+import com.kotlindiscord.kord.extensions.utils.addReaction
 import dev.kord.common.Color
-import dev.kord.common.entity.Permission
-import dev.kord.core.behavior.channel.createEmbed
+import dev.kord.core.behavior.edit
 import dev.kord.core.entity.channel.TextChannel
-import dev.kord.rest.builder.message.EmbedBuilder
-import dev.kord.rest.builder.message.EmbedBuilder.Limits.title
 import dev.kord.rest.builder.message.create.embed
+import dev.kord.rest.builder.message.modify.embed
 import io.github.maheevil.modbot.TEST_SERVER_ID
 import kotlinx.datetime.Clock
 
@@ -25,12 +23,12 @@ class SuggestionCommand : Extension() {
     override val name = "suggestion";
 
     override suspend fun setup() {
-        ephemeralSlashCommand {
+        publicSlashCommand {
             name = "suggestion"
             description = "suggestion related commands"
             guild(TEST_SERVER_ID)
 
-            publicSubCommand(::createSubCommandArgs){
+            publicSubCommand(::CreateSubCommandArgs){
                 name = "create"
                 description = "create a suggestion"
 
@@ -45,16 +43,13 @@ class SuggestionCommand : Extension() {
                             title = arguments.title
                             color = Color(0x09850b)
                             field("Suggestion",false){arguments.suggestionString}
+                            field("votes",false){"0"}
                             timestamp = Clock.System.now()
                         }
                     }.message.id
 
-                    /*val mesaageID = channel.createEmbed {
-                        title = arguments.title
-                        color = Color(0x09850b)
-                        field("Suggestion",false){arguments.suggestionString}
-                        timestamp = Clock.System.now()
-                    }.asMessage().id*/
+                    channel.getMessage(messageID).addReaction("\uD83D\uDC4D")
+                    channel.getMessage(messageID).addReaction("\uD83D\uDC4E")
 
                     (channel.asChannel() as TextChannel).startPublicThreadWithMessage(messageID,arguments.title).addUser(user.id)
                 }
@@ -62,7 +57,7 @@ class SuggestionCommand : Extension() {
         }
     }
 
-    inner class createSubCommandArgs : Arguments(){
+    inner class CreateSubCommandArgs : Arguments(){
         val suggestionString by coalescedString("suggestion", "the content of the suggestion")
         val title by defaultingCoalescingString("title", "Title at the top of the embed","Suggestion")
     }
